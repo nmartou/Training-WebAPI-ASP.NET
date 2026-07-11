3102d6f92ed81a209dd8b0acaa8abb4ffeb41b6a
[Nicolas] <martounicolas@gmail.com>
2026-07-11 11:24:13 +0200

    add SQLite context

--- Store/StoreContext.cs
+++ Store/StoreContext.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Store
 {
-    public class StoreContext
+    public class StoreContext : DbContext
     {
-        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Store.db");
+        }
     }
 }
