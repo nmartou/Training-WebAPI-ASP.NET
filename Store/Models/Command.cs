259f7d307091bfe477459ea0d77fe5c83588a2bb
[Nicolas] <martounicolas@gmail.com>
2026-07-11 12:29:26 +0200

    add command model

---
+++ Store/Models/Command.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Store.Models;
+
+class Command
+{
+    [Key]
+    public int CommandID { get; set; }
+
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "Quantity of product should be positive and not equal to zero")]
+    public int TotalQuanity { get; set; }
+
+    [Required]
+    [Range(0.0d, double.MaxValue, ErrorMessage = "Total price shoul be positive or zero")]
+    [DefaultValue(0.0d)]
+    public decimal TotalPrice { get; set; }
+
+    [Required]
+    [DefaultValue(false)]
+    public bool IsPaid { get; set; }
+
+    // public int InvoiceID { get; set; } // Theorical foreign key nullable
+
+    // public Invoice Invoice { get; set; }
+}
