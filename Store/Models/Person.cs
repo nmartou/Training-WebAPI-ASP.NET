699ee7fb4125780d42c2ca99cf25424d9580631b
[Nicolas] <martounicolas@gmail.com>
2026-07-11 12:42:47 +0200

    add Person model

---
+++ Store/Models/Person.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Store.Models;
+
+class Person
+{
+    [Key]
+    public int PersonID { get; set; }
+
+    [Required]
+    [StringLength(30)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string LastName { get; set; }
+
+    [Required]
+    public DateOnly BirthDate { get; set; }
+}
