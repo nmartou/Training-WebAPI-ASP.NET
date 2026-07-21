1c6981d1dd58c01b623a94678cf171590c2572c2
[Nicolas] <martounicolas@gmail.com>
2026-07-21 09:44:41 +0200

    Add interface for Price

---
+++ Store/Services/IPriceService.cs
@@ -0,0 +1,9 @@
+using Store.Models;
+
+namespace Store.Services;
+public interface IPriceService
+{
+    Task<Price?> GetPriceByIdAsync(int id);
+    Task<IEnumerable<Price>> GetAllPricesAsync();
+    Task<Price> CreatePriceAsync(Price price);
+}
