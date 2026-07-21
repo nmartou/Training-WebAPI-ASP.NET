1532a7840032ccf34d06bb92d4932e276f965ae3
[Nicolas] <martounicolas@gmail.com>
2026-07-21 09:43:05 +0200

    Add logging middleware to catch all exceptions

---
+++ Store/Middlewares/ExceptionLoggingMiddleware.cs
@@ -0,0 +1,23 @@
+public class ExceptionLoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionLoggingMiddleware> _logger;
+
+    public ExceptionLoggingMiddleware(RequestDelegate next, ILogger<ExceptionLoggingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occured while processing the request on {Path}.", context.Request.Path);
+        }
+    }
+}
