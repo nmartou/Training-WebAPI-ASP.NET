513a6c0e6b9c50f152dc58d4f601da68ed5b1198
[Nicolas] <martounicolas@gmail.com>
2026-07-21 09:44:22 +0200

    add global repository

---
+++ Store/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+
+namespace Store.Repositories;
+public interface IRepository<T>
+{
+    Task<T?> GetByIdAsync(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    Task<int> SaveChangesAsync();
+}
