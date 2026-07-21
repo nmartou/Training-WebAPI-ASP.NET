7aeb74a27c83ba5ad319b939c23205f994ad16b5
[Nicolas] <martounicolas@gmail.com>
2026-07-21 11:11:27 +0200

    add currency to price model

--- Store/Migrations/20260721074918_PriceUpdate.Designer.cs
+++ Store/Migrations/20260721074918_PriceUpdate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Store;
 
@@ -10,9 +11,11 @@ using Store;
 namespace Store.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20260721074918_PriceUpdate")]
+    partial class PriceUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.9");
